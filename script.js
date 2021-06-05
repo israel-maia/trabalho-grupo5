0f8819f01a7cc2fd5017d547511cf00dc58761a6
israel-maia <israelmaiasilva12@gmail.com>
2021-06-05 12:19:31 -0300

    imc

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function cauculoimc() {
-    var altura = parseFloat(document.getElementById('altura').value);
-    var peso = parseInt(document.getElementById('peso').value);
-    valor = peso / (altura * altura);
-    document.getElementById('resultado').value = valor.toFixed(2);
+    var altura = document.getElementById('altura').value;
+    var peso = document.getElementById('peso').value;
+    var valor = peso / (altura * altura);
+
+    if (altura !== '' && peso !== ''){
+        console.log('oi')
+
+        if (valor < 18.5) {
+            document.getElementById('resultado').value = valor.toFixed(2) + '' + ' Ta magrelo meu nobre';
+        }
+
+        else if (valor < 25.5) {
+            document.getElementById('resultado').value = valor.toFixed(2) + '' + ' Ta com o peso ideal meu nobre';
+        }
+
+        else {
+        document.getElementById('resultado').value = valor.toFixed(2) + '' + ' Ta gordao meu nobre';
+    }
+    }else{
+        document.getElementById('resultado').value = 'por favor preencha todos os campos'
+    
+    }
 }
